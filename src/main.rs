7fe6415a8ba70ca38902a27e63e236ee7139909b
Aditya <aditya26sg@gmail.com>
2023-12-06 19:32:23 +0530

    small update

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn get_mac_address() -> Result<String, MacAddressError> {
     let mac = mac_address::get_mac_address()?;
     match mac {
         Some(address) => {
-            println!("Mac address {:?}", address.to_string());
+            println!("\nMac address {:?}", address.to_string());
             Ok(address.to_string())},
         None => Err(MacAddressError::InternalError)
     }
@@ -21,7 +21,6 @@ fn get_mac_address() -> Result<String, MacAddressError> {
 
 #[tokio::main]
 async fn main()-> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
     let local_addr: SocketAddr = "0.0.0.0:8888".parse()?;
     let socket: UdpSocket = UdpSocket::bind(&local_addr).await?;
     socket.set_broadcast(true)?;
@@ -32,7 +31,7 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
 
     let socket: Arc<UdpSocket> = Arc::new(socket);
     let socket_for_broadcast: Arc<UdpSocket> = socket.clone();
-    println!("socket for broadcast {:?}", socket_for_broadcast);
+    println!("\nsocket for broadcast {:?}", socket_for_broadcast);
 
     tokio::spawn(async move {
         match get_mac_address() {
@@ -42,16 +41,16 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
                     node_name: node_name.clone(),
                     tcp_address
                 };
-                println!("Handshake message {:?}", msg);
+                println!("\nHandshake message {:?}", msg);
                 let serialized_message: String = serde_json::to_string(&msg).unwrap();
                 loop {
                     socket_for_broadcast.send_to(serialized_message.as_bytes(), BROADCAST_ADDR).await.unwrap();
-                    println!("Sending UDP broadcast...");
+                    println!("\nSending UDP broadcast...");
                     tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                 }
             },
             Err(error) => {
-                eprintln!("Error in fetching MAC address: {:?}", error);
+                eprintln!("\nError in fetching MAC address: {:?}", error);
             }
         }
     });
@@ -61,15 +60,22 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
         let listener: TcpListener = TcpListener::bind(("0.0.0.0", TCP_PORT)).await.unwrap();
         println!("TCP listener started {:?}", listener);
         while let Ok((stream, _)) = listener.accept().await {
-            println!("Accepted new TCP connection");
+            println!("\nAccepted new TCP connection");
             tokio::spawn(handle_tcp_stream(stream, nodes_clone.clone(), key_value_store.clone()));
         }
     });
 
+    // tokio::spawn(async move {
+    //     loop {
+    //         println!("\nTesting this spawn task\n");
+    //         tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    //     }
+    // });
+
     let mut buf = vec![0u8; 1024];
     loop {
         let (len, address) = socket.recv_from(&mut buf).await.unwrap();
-        println!("Received data on UDP from {}", address);
+        println!("\nReceived data on UDP from {}", address);
         let received_message: Message = serde_json::from_slice(&buf[..len])?;
         let local_node_name: String = get_mac_address()?;
 
@@ -78,7 +84,7 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
             if node_name == local_node_name {
                 continue;
             }
-            println!("Received handshake from node {:?}", node_name);
+            println!("\nReceived handshake from node {:?}", node_name);
             {
                 let mut nodes_guard = nodes.write().await;
                 nodes_guard.insert(node_name.clone(), NodeInfo { last_seen: Instant::now(), tcp_address });
